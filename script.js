1acb55f4d1a8e1843dabfe1a09688949fdcb8c55
DavideFolk <107867374+DavideFolk@users.noreply.github.com>
2025-01-27 18:26:49 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ let stringBase = 'abcdefghiklmnopqrstuvxyz';
 let numeri = '123456789';
 let stringMaiusc = 'ABCDEFGHIKLMNOPQRSTUVXYZ';
 let caratteri = '!=%&$?-.,';
-let password, rangeValue, stringFinale;
+let password, rangeValue, stringFinale, passPrecedente;
+let savedPass = [];
 
 function generaPassword() {
 	stringFinale = stringBase;
 	rangeValue = $('#customRange').val();
-	console.log(rangeValue);
 	password = '';
 	if ($('.numeri').is(':checked')) stringFinale += numeri;
 	if ($('.maiusc').is(':checked')) stringFinale += stringMaiusc;
@@ -17,31 +17,82 @@ function generaPassword() {
 		let num = Math.floor(Math.random() * stringFinale.length);
 		password += stringFinale.at(num);
 	}
+
 	$('.passwordText').text(password);
-	console.log(password);
 }
 
-/*
+// Slider dinamico
 $(document).on('input', '#customRange', function () {
 	rangeValue = $('#customRange').val();
-	let rangeExample = ''.padStart(rangeValue, '*');
-	$('.passwordText').text(rangeExample);
-	$('#testoOpzioni').text(`${rangeValue} caratteri`);
-	$('#testoOpzioni').show();
-	setTimeout(() => {
-		$('#testoOpzioni').fadeOut(1000);
-	}, 2000);
+	$('#slider-value').text(rangeValue);
 });
-*/
 
+// pulsante genera
 $('.genera').click(function (e) {
 	e.preventDefault();
 	generaPassword();
+	$('.save-btn').removeClass('invisible');
 });
 
-// Slider dinamico
-const slider = document.getElementById('customRange');
-const sliderValue = document.getElementById('slider-value');
-slider.addEventListener('input', (e) => {
-	sliderValue.textContent = e.target.value;
+// copia pass appunti
+$('.passwordText').click(function (e) {
+	e.preventDefault();
+
+	password = $('.passwordText').text();
+	if (password !== '*************' && password != passPrecedente) {
+
+		// Usa l'API Clipboard per copiare il testo
+		navigator.clipboard.writeText(password)
+			.catch((err) => {
+				console.error('Errore:', err);
+			});
+
+		// Mostra l'alert con effetto di fade-in
+		$('#update-alert')
+			.removeClass('invisible')
+			.fadeIn()
+			.find('span')
+			.text('Password copiata!');
+
+		// Nascondi l'alert dopo 2 secondi
+		setTimeout(function () {
+			$('#update-alert').fadeOut(function () {
+				$(this).addClass('invisible');
+			});
+		}, 2000);
+	}
+
+	passPrecedente = password;
 });
+
+// Gestione click sull'icona di salvataggio
+$('.save-btn').on('click', function () {
+	password = $('.passwordText').text();
+	if (!savedPass.includes(password)) savedPass.push(password);
+
+	// Mostra l'alert
+	$('#update-alert')
+		.removeClass('invisible')
+		.fadeIn()
+		.find('span')
+		.text('Password salvata!');
+
+	// Nascondi l'alert dopo 2 secondi
+	setTimeout(function () {
+		$('#update-alert').fadeOut(function () {
+			$(this).addClass('invisible');
+		});
+	}, 2000);
+});
+
+$('#modal_button').on('click', function () {
+
+	// Svuota il contenitore per evitare duplicati
+	$("#passwordList").empty();
+
+	// Aggiungi ogni password come elemento <p>
+	savedPass.forEach(password => {
+		$("#passwordList").append(`<p class="text-gray-200">${password}</p>`);
+	});
+});
+
